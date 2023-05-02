449309431696cd8fa8dd030237b79f612a53d1eb
DESKTOP-9AP00PL\user <orihy@gmail.com>
2023-05-02 14:43:42 +0900

    선생님들 제발요

--- src/main/java/com/ohgiraffers/level01/basic/Application2.java
+++ src/main/java/com/ohgiraffers/level01/basic/Application2.java
@@ -3,7 +3,10 @@ package com.ohgiraffers.level01.basic;
 public class Application2 {
 
 	public static void main(String[] args) {
-		
+		float width = 12.5F;
+		float height = 36.4F;
+		System.out.println(width*height);
+		System.out.println((width+height)*2);
 		/* 사각형의 넓이와 둘레를 구하는 문제이다.
 		 * 너비 12.5 높이 36.4를 변수에 저장하고
 		 * 각 넓이와 둘레를 계산해 변수에 담고 아래와 같이 출력되도록 하시오
