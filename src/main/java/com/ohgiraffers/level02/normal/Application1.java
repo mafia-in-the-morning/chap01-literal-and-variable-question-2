aafcc9047e9e5bf5d90ca953777d33efcee00bb1
고동환\eurdr <eurdream98@naver.com>
2023-05-02 14:42:33 +0900

    문제3 해결 완료!

--- src/main/java/com/ohgiraffers/level02/normal/Application1.java
+++ src/main/java/com/ohgiraffers/level02/normal/Application1.java
@@ -6,14 +6,12 @@ public class Application1 {
 
 		/* 문자형 변수를 하나 선언 후 'a'를 대입한 후
 		 * 출력은 문자 'a'가 가지는 유니코드값을 출력하도록 하세요
-		 * 
+		 *
 		 * -- 출력 예시 --
 		 * 문자 a의 unicode : 97
 		 * */
-
 		char a=97;
 		System.out.println("문자형 'a'의 unicode"+a);
-		System.out.println();
 	}
 
 }
